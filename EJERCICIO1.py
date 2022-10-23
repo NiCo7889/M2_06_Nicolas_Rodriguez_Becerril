1c0496b42f5713ddaeef8b8b1a4b2fee95dbfe1a
NiCo7889 <113980882+NiCo7889@users.noreply.github.com>
2022-10-23 19:20:13 +0200

    cambio

---
+++ EJERCICIO1.py
@@ -0,0 +1,9 @@
+import math
+math.pi
+def area_circulo():
+    area =  math.pi*radio^2
+    return
+
+radio = int(input("Dime el radio de un círculo: "))
+area = area_circulo()
+print("El área del círculoa es: ", area )
